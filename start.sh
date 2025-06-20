23012c4a8d0fbf5266595b50c17e6871b0c488a7
QuantumPhantom09 <164841542+QuantumPhantom09@users.noreply.github.com>
2025-06-20 09:52:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 echo "Starting Coqui TTS server..."
 
 python3 -m TTS.server.server \
-    --model_name "tts_models/multilingual/multi-dataset/xtts_v2" \
+    --model_name "tts_models/en/ljspeech/tacotron2-DDC" \
     --speaker_wav "" \
     --port $PORT \
     --host 0.0.0.0
